2ad1cbe06a426c28d4f284eab7427edf748a8dd2
Codesforstuff <Tylerlpeng@gmail.com>
2020-07-29 16:47:41 -0700

    steam api test 2

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+t = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+ request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+ request.onload = function () {
+   // Begin accessing JSON data here
+   }
+
+   // Send request
+request.send()
+</script>

b02b875d3f49a705f99c3fc55636bc0076f2b8d1
Codesforstuff <Tylerlpeng@gmail.com>
2020-07-29 16:53:48 -0700

    Steam api test 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 t = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
- request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+ request.open('GET', 'http://steamcommunity.com/inventory/76561198291496208/440/2?l=english&count=5000', true)
 
  request.onload = function () {
    // Begin accessing JSON data here
